97c472f0c4744d1ef8ec7445fb51c9e065fd8895
reactguysinfotech <reactguys.infotech@gmail.com>
2024-05-20 20:37:32 +0530

    Fixed Engine

--- data/database.js
+++ data/database.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = () => {
   mongoose
     .connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME })
-    .then(() => {
-      console.log("Database Connected");
+    .then((c) => {
+      console.log(`Database Connected with ${c.connection.host}`);
     })
     .catch((e) => {
       console.log(e);
